65153fece9adb6718cac3e15a4a87fc33f212a70
mtontsch <maximilian.tontsch@gmx.de>
2026-08-06 15:07:03 +0200

    planner update v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "eclipse-v5";
+const CACHE = "eclipse-v3";
 const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
